4eb14f58c1ac42b306722e57e580aaa2288cd2f4
yuan <yuan@yuantekiMacBook-Pro.local>
2015-12-16 16:44:21 -0500

    yuan model-based-classification

--- model_based_online_kernel_classification.m
+++ model_based_online_kernel_classification.m
@@ -1,55 +1,56 @@
-function main()
-C = 0.8;
-r = 0.001;
-rho = 1.2;
-model_based_classification(C,r,rho);
-end
 
-function model_based_classification(C,r,rho)%model based classification
-    %input vectors%
-    load('mnist_49_3000.mat');
-    x = x';
-    y = y';
-    x = x(1:1500, :);
-    y = y(1:1500, :);
-    n = size(x,1);
-    kernel_sigma = 16;
-    %define tunning parameter%
-    %C = 10%0.8;
+ 
+function model_based_online_kernel_classification(C,r,rho,x,y,kernel_sigma)%model based classification
+    %tunning parameter set to be :
+    %C = 1; 
     %r = 0.001;
     %rho = 1.2;
-    
+    %kernel_sigma = 1600;
+    %change one of the class to -1
+    indices = randperm(size(x,1));
+    x = x(indices, :);
+    y = y(indices, :);
+    n = size(x,1);
+   
     alphas = [];
     t = 1;
     correct = 0;
     while t <= n
         if t == 1
-            alpha_new = get_new_alpha(C,r,y(t,:),0);
-            f = alpha_new;
+            
+            f = 0 ;
+            alpha_new = get_new_alpha(C,r,y(t,:),f);
+        
             alphas = [alphas;alpha_new];
         else
-            %get f_t(x_{t})
+            %get f_t(x_{t+1})
             k_mat = kernel_gaussian(x(1:t-1, :), x(t, :), kernel_sigma);
-            f = alphas'*k_mat;
-            %get alpha_{tt}
+            f = alphas'*k_mat;%eqn 31
+            
+            %get alpha_{t+1}
             alpha_new = get_new_alpha(C, r, y(t,:),f);
+            
             %update the old alphas first
-            alphas = alphas.*(1/(1+r));
+            alphas = alphas/(1+r);%eqn 33
+            %put alpha_{t+1} into alphas
             alphas = [alphas;alpha_new];
         end
+        %prediction error
         if f*y(t,:) > 0
             correct = correct + 1;
         end
+        %fprintf('f = %f, y = %d, t = %d, correct= %d\n',f, y(t,:),t,correct);
+       
         t = t + 1;
-        fprintf('%d, %d\n',t,correct);
     end
-    fprintf('C = %f,r = %f,rho = %f, correct = %f\n',C,r,rho,correct/n);
+    fprintf('C = %f,r = %f,rho = %f, errors = %d out of %d\n',C,r,rho,t - correct -1, t -1);
 
 end
 
 function new_alpha = get_new_alpha(C,r,y,f)
-    alpha_bar = 1 + r - y*f;
-    if alpha_bar < 0
+    alpha_bar = 1 + r - y*f;%sentence after eqn 30
+    %eqn 33
+    if alpha_bar <= 0
         new_alpha = 0;
     else
         if alpha_bar >= C
